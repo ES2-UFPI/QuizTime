a8a2c562301fd4aea746e7c660d84bfb0067a021
SSantos425 <samuel.tri@hotmail.com>
2022-08-22 20:55:53 -0300

    fix homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
-
-ActiveRecord::Schema.define(version: 2022_08_18_140444) do
-
-
+ActiveRecord::Schema.define(version: 2022_08_22_224319) do
 
   create_table "alunos", force: :cascade do |t|
     t.string "nome"
@@ -26,7 +20,13 @@ ActiveRecord::Schema.define(version: 2022_08_18_140444) do
     t.datetime "updated_at", null: false
   end
 
-
+  create_table "professors", force: :cascade do |t|
+    t.string "nome"
+    t.string "graduacao"
+    t.string "matricula"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,7 +39,5 @@ ActiveRecord::Schema.define(version: 2022_08_18_140444) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-
 
 end
