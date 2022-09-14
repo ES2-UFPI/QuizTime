9c489843e5ec2308823fc774d3b1766c9052600d
Lucas William <lucaswilliam133@gmail.com>
2022-09-13 21:06:13 -0300

    delete peding test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper' 
-describe User do 
- pending "add some examples to (or delete) #{__FILE__}" 
-end
 
 class User < ApplicationRecord
     validates :first_name, presence: true 
     validates :last_name, presence: true 
     validates :email, presence: true 
     validates :password, presence: true 
-    # validates :email, presence: true, uniqueness: true 
 end
 
 describe User do 
