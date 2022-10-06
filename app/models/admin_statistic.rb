8ad9a0f1438daefc96943aec1307ffffe77022e5
Lucas William <lucaswilliam133@gmail.com>
2022-10-05 22:16:29 -0300

    refatorando código #61

--- app/models/admin_statistic.rb
+++ app/models/admin_statistic.rb
@@ -4,9 +4,10 @@ class AdminStatistic < ApplicationRecord
         total_questions: "TOTAL_QUESTIONS"
     }
 
-    def self.set_event(event)
-        admin_statistic = AdminStatistic.find_or_create_by(event: event)
-        admin_statistic.value += 1
-        admin_statistic.save
-    end
+    scope :total_users, -> {
+        find_by_event(EVENTS[:total_users]).value
+    }
+    scope :total_questions, -> {
+        find_by_event(EVENTS[:total_questions]).value
+    }
 end
